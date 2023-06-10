ff01ccaabd0bb5efe703453f4953ac546d7a7215
goldietiara <goldietiara.acc@gmail.com>
2023-06-10 10:18:36 +0700

    Private API Calls to Get or Create user

--- backend/index.js
+++ backend/index.js
@@ -7,25 +7,21 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
+//Get or Create user object in our chat ensigne database
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
 
-  //Get or Create Chat
+  //private API calls
   try {
-    const r = await axios.put("https://api.chatengine.io/chats/");
-    {
-      username: username;
-      secret: username;
-      fristname: username;
-    }
-    {
-      Headers: {
-        ("private-key: 97042a56-8cac-4fd7-8ae4-a12fbbb4def2");
-      }
-    }
-  } catch (e) {}
-
-  return res.json({ username: username, secret: "sha256..." });
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, fristname: username },
+      { headers: { "private-key": "97042a56-8cac-4fd7-8ae4-a12fbbb4def2" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
 app.listen(3001);
